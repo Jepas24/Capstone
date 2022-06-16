b89f6458de4600df70e03d2b7d49d9dbe5301556
JerriemyPascual <v-jerriemypa@cloudstaff.com>
2022-06-16 09:58:10 +0800

    commit

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -20,6 +20,20 @@ const Footer = () => {
           <i className="fa fa-twitter icon"></i>
           <i class="fa fa-youtube icon"></i>
        </div>
+       <div className='ams_footer-content'>
+          <div className='ams_footer-content-aboutus'>
+              <p>An About Us page helps your company make a good first impression, and is critical for building customer trust and loyalty.
+                 An About Us page should make sure to cover basic information about the store and its founders, explain the company's purpose 
+                 and how it differs from the competition, and encourage discussion and interaction.
+                 Here are some free templates, samples, and example About Us pages to help your ecommerce store stand out from the crowd</p>
+          </div>
+          <div className='ams_footer-content-contactus'>
+              <p>An About Us page helps your company make a good first impression, and is critical for building customer trust and loyalty.
+                 An About Us page should make sure to cover basic information about the store and its founders, explain the company's purpose 
+                 and how it differs from the competition, and encourage discussion and interaction.
+                 Here are some free templates, samples, and example About Us pages to help your ecommerce store stand out from the crowd</p>
+          </div>
+       </div>
        <div className='ams_footer-box'>
         <div className='ams__Footer-text'>
         <p>@2022 | City College Of San Fernando Pampanga</p>
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './header-Home.css';
 import { Link } from 'react-router-dom';
 const Home = () => {
+  const [modal, setModal] = useState(false);
+  const Login = () => {
+    setModal(!modal);
+      };
   return (
+    
     <div className='ams__header' id='CloudOverDrive'>
          <div className='ams__header-content'>
             <h1>Monitoring and Control System</h1>
          </div>
-         <Link to="/OverDrive/A"  target="_blank"> <button className="ams__header-button button2">Log in Account</button> </Link>
+         <button className="ams__header-button button2" onClick={Login}>Log in Account</button>
+         {modal && (
+            <div className="modal">
+              <div onClick={Login} className="overlay"></div>
+                <div className="modal-content">
+                  <h2>Log in Account</h2>
+                      <form >
+                        <div className="input-container">
+                          <i className="fa fa-sign-in iconm"></i>
+                          <input type="text" name="name"/>
+                        </div>
+                    <div className="input-container">
+                       <i className="fa fa-key iconm"></i>
+                   <input type="text" name="name" />
+                    </div> 
+                   <button type="submit" className="btn">Submit</button>
+              </form>
+                   <button className="close-modal" onClick={Login}>
+                   <i className="fa fa-window-close-o iconm"></i>
+              </button>
+          </div>
+        </div>
+        )}
     </div>
   )
 }
