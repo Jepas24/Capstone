c3d3a222a8833752e7436309ffa20860c76b608e
JerriemyPascual <v-jerriemypa@cloudstaff.com>
2022-06-21 19:28:24 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Navbar,Footer,Home} from './components';
+import {AdminDashboard} from './Container';
 import { BrowserRouter as Router, Route, Switch,Redirect  } from 'react-router-dom';
 import { Refreshable } from 'react-router-refreshable'
 import './App.css';
@@ -9,25 +10,30 @@ const App = () => {
   return (
     <Router>
     <div className='App'>
-      <div className='gradient_bg'>
-          <Navbar />
-      </div>
-      
           <Switch>
-            <div className='ams_backgrund-home'>
                 {/* This Route is for Song Play */}
                  <Route exact path="/">
                     <Redirect to="/Home" />
                   </Route>
                   <Route exact path="/Home">
+                  <div className='gradient_bg'>
+                    <Navbar />
+                  </div>
+                  <div className='ams_backgrund-home'>
                   <Refreshable>
                         <Home />
                   </Refreshable>
+                  </div> 
+                  <Footer />
                   </Route>
-            </div>      
+                
           </Switch>
-          
-          <Footer />
+          <Switch>
+                {/* This Route is for Song Play */}
+                 <Route exact path="/d">
+                     <AdminDashboard />
+                  </Route>
+          </Switch>
     </div>
     </Router>
   )
