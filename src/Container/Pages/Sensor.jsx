e1cc253a5dadc278962b006522d9c90cffaf3cc9
JerriemyPascual <v-jerriemypa@cloudstaff.com>
2022-07-12 20:50:16 +0800

    commit

--- src/Container/Pages/Sensor.jsx
+++ src/Container/Pages/Sensor.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import './dashboard.css';
 const Sensor = () => {
   return (
     <div className='sensor'>
-        <h1>Sensor</h1>
+        <iframe  src="https://thingspeak.com/channels/1795138/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&timescale=10&type=step"></iframe>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
 import App from './App';
-import {AuthProvider} from './Context/AuthProvider';
+
 
 import './index.css'
 
 ReactDOM.render(
-<AuthProvider>
+
     <App/>
-</AuthProvider>
+
 ,document.getElementById('root'))
